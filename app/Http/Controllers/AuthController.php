fcef51f11c0609d8bbb277652a85ce395ab168cc
Vadim Solobchuk <mysolobchuk@gmail.com>
2023-05-21 18:18:54 +0300

    .

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -56,7 +56,7 @@ class AuthController extends Controller
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
-            'user' => $user,
+            'user' => $user->fresh(),
             'access_token' => $token,
             'token_type' => 'Bearer',
         ]);
